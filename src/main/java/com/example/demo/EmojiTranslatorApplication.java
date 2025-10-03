3342b4747fccd777b94a36cb087730c8a7875ab3
shivani172454 <shashidharshivani88@gmail.com>
2025-10-03 15:03:56 +0530

    main file structure

---
+++ src/main/java/com/example/demo/EmojiTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmojiTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmojiTranslatorApplication.class, args);
+	}
+
+}
