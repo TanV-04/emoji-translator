b8a42cab1c717a4bea10cc3e262955ad81bc7da1
ShilpiBiswal <biswalshilpi@gmail.com>
2025-10-09 22:33:51 +0530

    Linked favorite emoji functionality with NoAI emoji translation

--- src/main/java/com/example/demo/repository/FavoriteEmojiRepository.java
+++ src/main/java/com/example/demo/repository/FavoriteEmojiRepository.java
@@ -1,11 +1,8 @@
 package com.example.demo.repository;
 
 import com.example.demo.entity.FavoriteEmoji;
-import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
 
 public interface FavoriteEmojiRepository extends JpaRepository<FavoriteEmoji, Long> {
-    List<FavoriteEmoji> findByUser(User user);
+    FavoriteEmoji findByName(String name);
 }
-
